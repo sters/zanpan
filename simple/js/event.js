6d10d00412647ce086fbbdde976aad5db60e9fc2
sters <sters@users.noreply.github.com>
2013-03-04 13:55:19 +0900

    Add event class.
    I was goint to make a generic.
    Not only use jsBefunge, can use anything
    
    But, cannot use in addEventListner ( or jQuery: on(~~) ) function.
    umm, To work with these, use CustomEvent ?
    (document.createEvent, dispatchEvent, ...other)

---
+++ simple/js/event.js
@@ -0,0 +1,54 @@
+
+(function($jb) {
+    
+    /**
+     * Befunge EventManage class
+     * @constructor
+     */
+    $jb.EventManage = function(){
+        this.events = {};
+    };
+    
+    /**
+     * Event define. jQuery like named
+     * @param {string} Target string. It use for events manage
+     * @param {function} Event's call this method
+     * @return {integer} 
+     */
+    $jb.EventManage.prototype.on = function(target, func) {
+        if(!this.events[target])
+            this.events[target] = [];
+        this.events[target].push(func);
+        return this.events[target].length - 1;
+    };
+    
+    /**
+     * Event undefine. jQuery like named
+     * @param {string} Target string
+     * @param {integer} Event's ID
+     */
+    $jb.EventManage.prototype.off = function(target, eventID) {
+        if(!this.events[target]) return;
+        this.events[target][eventID] = null;
+    };
+    
+    /**
+     * Event fire.
+     * @param {target} Target string
+     * @param {object} Event call in this
+     */
+    $jb.EventManage.prototype.fire = function(target, _this) {
+        if(!this.events[target]) return;
+        for(var _i=0, _m=this.events[target].length; _i < _m; _i++) {
+            var event = this.events[target][_i];
+            event.call(_this, target, _this);
+        }
+    };
+    
+    /**
+     * Event management instance
+     * @type {jsBefunge.EventManage}
+     */
+    $jb.Event = new $jb.EventManage();
+    
+})(jsBefunge);
