bf240b2cf89b39458f549ef8535c29b76bc9b65e
sters <sters@users.noreply.github.com>
2013-03-05 04:05:05 +0900

    Add Event.fire in InputBuffer and Result functions
     "new Function()" is nice item!
    
    umm, codes is not "DontRpeatYourself"
    to similar these code and "Stack.js" event fire

--- simple/js/input_buffer.js
+++ simple/js/input_buffer.js
@@ -11,5 +11,14 @@
     };
     $jb.InputBuffer.prototype = new $jb.Buffer();
     $jb.InputBuffer.prototype.__super = $jb.Buffer.prototype;
-
+    
+    // function override for event call
+    var fnames = ["clear", "set"]
+    var funcs = {};
+    for(var _i = 0, _m = fnames.length; _i < _m; _i++) {
+        var _item = fnames[_i];
+        funcs[_item] = new Function("this.__super." + _item + ".apply(this, arguments);");
+    }
+    $jb.Event.defineFire("InputBuffer", funcs);
+    
 })(jsBefunge);
