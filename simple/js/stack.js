d3bf081316374e2312988d7ed5f91105ec3e7a3c
sters <sters@users.noreply.github.com>
2013-03-04 23:08:46 +0900

    Add push, pop in Stack define
    for Event fire system...

--- simple/js/stack.js
+++ simple/js/stack.js
@@ -18,6 +18,20 @@
      */
     $jb.Stack.prototype.clear = function() {
         this.length = 0;
+        $jb.Event.fire("Stack.clear", this);
     };
-        
+    
+    // function override for event call
+    var overrides = ["push", "pop"];
+    var clsname   = "Stack";
+    for(var _i=0, _m=overrides.length; _i<_m; _i++) {
+        var _item = overrides[_i];
+        eval(
+            "$jb." + clsname + ".prototype." + _item + "= function() {" +
+                "this.__super." + _item + ".apply(this, arguments);" +
+                "$jb.Event.fire('" + clsname + "." + _item + "', this);" +
+            "};"
+        );
+    }
+    
 })(jsBefunge);
