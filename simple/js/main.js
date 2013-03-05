61fa760d475a5080756c256ea35ac05c22432a70
sters <q.sters.p@gmail.com>
2013-03-06 05:55:15 +0900

    Fix mainnnnnnnnn
    ah.. forget separate in commit..... fix points are in bulk commit......

--- simple/js/main.js
+++ simple/js/main.js
@@ -1,160 +1,231 @@
 
 var jsBefunge = {};
 
+var _befunge;
+
 (function($) {
     
     $.fn.SetBefunge = function() {
         
-        // elements
-        var BefungeElements = {
-            Code        : "#befungeCode",
-            Input       : "#befungeInput",
-            RunCode     : "#befungeRunCode",
-            Stack       : "#befungeStack",
-            Result      : "#befungeResult",
-            InputArea   : "#befungeInputArea"
+        // dom use element ids
+        var _elementIDs = {
+            Code        : "befungeCode",
+            Input       : "befungeInput",
+            RunCode     : "befungeRunCode",
+            Stack       : "befungeStack",
+            Result      : "befungeResult",
+            InputArea   : "befungeInputArea"
         };
-        
-        // init, add "Befunge" components
+
+        // Befunge object
+        _befunge = new jsBefunge.Code();
+                
+        // befunge element
         var element = $(this);
-        (function() {
+        element.html("");
+        
+        // setup UI
+        var txtCode = $("<textarea id='" + _elementIDs.Code + "'>"),
+            txtBox  = $("<input type='text' id='" + _elementIDs.Input + "'>"),
+            btRun   = $("<input type='button' value='Run'>"),
+            btStep  = $("<input type='button' value='Step'>"),
+            btPause = $("<input type='button' value='Pause'>"),
+            btAbord = $("<input type='button' value='Abord'>"),
+                       
+            // add befunge events
+            addBefungeEvents = function(){
             
-            // add stylesheet
-            $("head:first").append(
-                '<style>' +
-                    '\n ' + BefungeElements.Code +
-                        ' {float:left; width:100%; height:300px;}' +
-                    '\n ' + BefungeElements.RunCode +
-                        ' {float:left; border-collapse: collapse;}' +
-                    '\n ' + BefungeElements.RunCode + ' td' +
-                        ' {width:12px; height:20px; border:1px solid gray; text-align:center;}' +
-                    '\n ' + BefungeElements.RunCode + ' td.now' +
-                        ' {background-color: #BCF3FF;}' +
-                    '\n ' + BefungeElements.RunCode + ' td.break' +
-                        ' {background-color: #FF9393;}' +
-                    '\n ' + BefungeElements.RunCode + ' td.now.break' +
-                        ' {background-color: #DEC3C9;}' +
-                    '\n ' + BefungeElements.Stack +
-                        ' {overflow:hidden; float:left; padding:5px; border:1px solid black; width:100px; height:300px;}' +
-                    '\n ' + BefungeElements.Result +
-                        ' {margin-bottom:30px;}' +
-                    '\n ' + BefungeElements.Input +
-                        ' {width:300px;}' +
-                    '\n ' + BefungeElements.InputArea +
-                        ' {}' +
-                '\n</style>'
-            );
-            
-            // BefungeScripts Deferred Loading
-            var $head = $("head:first");
-            var script_load = function(scripts) {
-                if(scripts.length == 0) return;
-                var script = document.createElement("script");
-                script.setAttribute("type", "text/javascript");
-                script.setAttribute("src", scripts.shift());
-                script.onload = function() {
-                    script_load(scripts);
+                // code changed call event
+                var onCodePositionChanged = function(remove) {
+                    $("#" + _elementIDs.RunCode +
+                      " tr:eq(" + _befunge.pos[1]  + ") td:eq(" + _befunge.pos[0] + ")")
+                        .toggleClass("now", !remove);
                 };
-                $("head")[0].appendChild(script);
-            };
-            script_load(["js/input_buffer.js", "js/result.js", "js/stack.js", "js/code.js", "js/event.js"]);
+                
+                // Stack action
+                {
+                    var onStackChanged = function(t) {
+                        $("#" + _elementIDs.Stack).html( t.reverse().join("<br>") );
+                    };
+                    
+                    _befunge._event.on("Stack.push",  onStackChanged);
+                    _befunge._event.on("Stack.pop",   onStackChanged);
+                    _befunge._event.on("Stack.clear", onStackChanged);
+                }
+                
+                
+                // Result action
+                {
+                    var onResultChanged = function(t) {
+                        $("#" + _elementIDs.Result).html( t.buffer );
+                    };
+
+                    _befunge._event.on("Result.add",   onResultChanged);
+                    _befunge._event.on("Result.set",   onResultChanged);
+                    _befunge._event.on("Result.clear", onResultChanged);
+                }
+
+                // Code action
+                {
+                    // step
+                    _befunge._event.on("Code.beforeStep", function(t,v){
+                        onCodePositionChanged(true);
+                    });
+                    _befunge._event.on("Code.step", function(t,v){
+                        onCodePositionChanged();
+                    });
+
+                    // code stop
+                    //_befunge._event.on("Code.stop", function(t,v){ console.log("Code.stop"); console.log(t.Result.buffer); });
+                
+                    // init event => make table
+                    _befunge._event.on("Code.init", function(t,v){
+                        var $runcode = $("#" + _elementIDs.RunCode)
+                        $runcode.text("");
+                        for(var i=0, ymax=this.source.length; i<ymax; i++) {
+                            var tr = $("<tr>");
+                            for(var j=0, xmax=this.source[i].length; j<xmax; j++) {
+                                tr.append(
+                                    $("<td>").text(this.source[i].substr(j,1))
+                                );
+                            }
+                            $runcode.append(tr);
+                        }
+                        $runcode.find("td").dblclick(function(){
+                            $(this).toggleClass("break");
+                        });
+                        onCodePositionChanged();
+                    });
+                }
+            },
             
-            // set tags
-            element.html("");
-            var txtBox = $("<input type='text' id='" + BefungeElements.Input.substr(1) + "'>");
-            var btSend = $("<input type='button' value='Run'>");
-            var btStep = $("<input type='button' value='Step'>");
-            var btPause = $("<input type='button' value='Pause'>");
-            var btAbord = $("<input type='button' value='Abord'>");
-            
-            // add events
-            btSend.click(function(){
-                btSend.attr("disabled", "true");
-                btStep.removeAttr("disabled");
-                btPause.removeAttr("disabled");
-                btAbord.removeAttr("disabled");
-                $(BefungeElements.Input).attr("disabled","disabled");
-                $("html,body").animate({
-                    scrollTop: $(BefungeElements.RunCode).offset().top
-                }, 0);
-                (new jsBefunge.Code).run( $(BefungeElements.Code).val(), function(code) {
-                    console.log(code.Result.buffer);
-                });
-            });
-            btStep.click(function(){
-                if(Code.running) {
-                    Code.Step();
-                } else {
-                    btSend.attr("disabled", "true");
+            // add UI events
+            addUIEvents = function() {
+                
+                // init UI
+                var initUI = function() {
+                    txtBox.attr("disabled", "disabled");
+                    btRun.attr("disabled", "disabled");
+                    btStep.attr("disabled", "disabled");
                     btPause.removeAttr("disabled");
                     btAbord.removeAttr("disabled");
-                    InputBuffer.DisableBox();
-                    $("html,body").animate({
-                        scrollTop: $(BefungeElements.RunCode).offset().top
-                    }, 0);
-                    Code.Start();
-                }
-                btAbord.removeAttr("disabled");
-            });
-            /*Code.BreakPoint = function() {
-                Code.Stop();
-                btStep.removeAttr("disabled");
-                btPause.val("Resume");
-            };*/
-            btPause.click(function(){
-                if($(this).val() == "Pause") {
-                    Code.BreakPoint();
-                } else {
-                    Code.Run();
-                    btStep.attr("disabled", "true");
-                    btPause.val("Pause");
-                }
-            }).attr("disabled", "true");
-            btAbord.click(function(){
-                btPause.click();
-                Code.Stop(true);
-                InputBuffer.EnableBox();
-                btSend
-                    .removeAttr("disabled")
-                    .removeClass("stateInputCommand")
-                    .removeClass("stateInputName")
-                    .removeClass("stateInputText");
-                //btStep.attr("disabled", "true");
-                btPause
-                    .attr("disabled", "true")
-                    .val("Pause");
-                btAbord.attr("disabled", "true");
-                InputBuffer.EnableBox();
-            }).attr("disabled", "true");
-            
-            // add tags
-            element
-                .append(
-                    "<textarea id='" + BefungeElements.Code.substr(1) + "'></textarea>"
-                )
-                .append(
-                    $("<span><span>Input</span></span>")
-                        .append(txtBox)
-                        .attr("id", BefungeElements.InputArea.substr(1))
-                )
-                .append(btSend)
-                .append("&nbsp;&nbsp;")
-                .append(btStep)
-                .append("&nbsp;&nbsp;")
-                .append(btPause)
-                .append("&nbsp;&nbsp;&nbsp;&nbsp;")
-                .append(btAbord)
-                .append(
-                    "<br>" +
-                    "<div id='" + BefungeElements.Stack.substr(1) + "'></div>" +
-                    "<table id='" + BefungeElements.RunCode.substr(1) + "'></table>" +
-                    "<hr style='clear:both;width:100%;'>" +
-                    "<div id='" + BefungeElements.Result.substr(1) + "'></div>"
-                )
-            ;
-        })();
+                    $("html,body").css( "scrollTop", $("#" + _elementIDs.RunCode).offset().top );                
+                };
+                
+                // run button
+                btRun.click(function(){
+                    initUI();
+                    _befunge.run( $("#" + _elementIDs.Code).val() );
+                });
+                
+                // step button
+                btStep.click(function(){
+                    if(_befunge.running) {
+                        _befunge.step();
+                    } else {
+                        initUI();
+                        btPause.val("Resume");
+                        _befunge.run( $("#" + _elementIDs.Code).val(), true );
+                    }
+                    btAbord.removeAttr("disabled");
+                });
+                
+                // pause button
+                btPause.click(function(){
+                    if($(this).val() == "Pause") {
+                        _befunge.stop();
+                        btStep.removeAttr("disabled");
+                        btPause.val("Resume");
+                        
+                    // if text is resume
+                    } else {
+                        _befunge.run();
+                        btStep.attr("disabled", "true");
+                        btPause.val("Pause");
+                    }
+                }).attr("disabled", "true");
+                
+                // abord button
+                btAbord.click(function(){
+                    _befunge.stop(true);
+                    txtBox.removeAttr("disabled");
+                    btRun.removeAttr("disabled");
+                    btStep.removeAttr("disabled");
+                    btPause.attr("disabled", "true").val("Pause");
+                    btAbord.attr("disabled", "true");
+                }).attr("disabled", "true");
+            };
         
-        return this; // jsBefunge method chain
+        // event setup
+        addBefungeEvents();
+        addUIEvents();
+        
+        // add stylesheet
+        $("head:first").append(
+            '<style>' +
+                '\n #' + _elementIDs.Code +
+                    ' {float:left; width:100%; height:200px;}' +
+                '\n #' + _elementIDs.RunCode +
+                    ' {float:left; border-collapse: collapse;}' +
+                '\n #' + _elementIDs.RunCode + ' td' +
+                    ' {width:12px; height:20px; border:1px solid gray; text-align:center;}' +
+                '\n #' + _elementIDs.RunCode + ' td.now' +
+                    ' {background-color: #BCF3FF;}' +
+                '\n #' + _elementIDs.RunCode + ' td.break' +
+                    ' {background-color: #FF9393;}' +
+                '\n #' + _elementIDs.RunCode + ' td.now.break' +
+                    ' {background-color: #DEC3C9;}' +
+                '\n #' + _elementIDs.Stack +
+                    ' {overflow:hidden; float:left; padding:5px; border:1px solid black; width:100px; height:300px;}' +
+                '\n #' + _elementIDs.Result +
+                    ' {margin-bottom:30px;}' +
+                '\n #' + _elementIDs.Input +
+                    ' {width:300px;}' +
+                '\n #' + _elementIDs.InputArea +
+                    ' {}' +
+            '\n</style>'
+        );
+        
+        // BefungeScripts Deferred Loading
+        /*var $head = $("head:first");
+        var script_load = function(scripts) {
+            if(scripts.length == 0) return;
+            var script = document.createElement("script");
+            script.setAttribute("type", "text/javascript");
+            script.setAttribute("src", scripts.shift());
+            script.onload = function() {
+                script_load(scripts);
+            };
+            $("head")[0].appendChild(script);
+        };
+        script_load(["js/input_buffer.js", "js/result.js", "js/stack.js", "js/code.js", "js/event.js"]);
+        */
+        
+        // add tags
+        element
+            .append(txtCode)
+            .append(
+                $("<span><span>Input</span></span>")
+                    .append(txtBox)
+                    .attr("id", _elementIDs.InputArea)
+            )
+            .append(btRun)
+            .append("&nbsp;&nbsp;")
+            .append(btStep)
+            .append("&nbsp;&nbsp;")
+            .append(btPause)
+            .append("&nbsp;&nbsp;&nbsp;&nbsp;")
+            .append(btAbord)
+            .append(
+                "<br>" +
+                "<div id='" + _elementIDs.Stack + "'></div>" +
+                "<table id='" + _elementIDs.RunCode + "'></table>" +
+                "<hr style='clear:both;width:100%;'>" +
+                "<div id='" + _elementIDs.Result + "'></div>"
+            )
+        ;
+        
+        return this; // jsBefunge method chain...lol.....
     };
     
 })(jQuery);
